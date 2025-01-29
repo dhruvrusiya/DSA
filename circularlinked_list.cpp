c09423e5e69253846aee9f838a13cf15284ac68f
dhruv rusiya <dhruvrusiya070@gmail.com>
2025-01-29 19:47:38 +0530

    cll

--- circularlinked_list.cpp
+++ circularlinked_list.cpp
@@ -1,13 +1,61 @@
-#include <iostream>;
+#include <iostream>
 using namespace std;
 struct Node{
     int data;
     Node * next;
-
+   
+};
+Node * head = NULL;
+void insert(int x)
+{
+    Node *newNode = new Node;
+    newNode->data = x;
+   
+    if(head == NULL)
+    {
+        head = newNode;
+        newNode->next = head;
+    }
+    else
+    {
+        Node *temp = head;
+        while(temp->next != head)
+        {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+        newNode->next = head;
+        head = newNode;
+       
+    }
+   
 }
+void show()
+    {
+        cout<<"data is here: "<<endl;
+        Node *temp = head;
+        while(temp->next != head)
+        {
+            cout<<temp->data<<" ";
+            temp = temp->next;
+        }
+        cout<<temp->data<<" ";
+    }
+   
 
-
-
-int main(){
-    return 0;
+int main()
+{
+    insert(11);
+    insert(12);
+    insert(13);
+    insert(14);
+    insert(15);
+     insert(16);
+    insert(17);
+    insert(18);
+    insert(19);
+    insert(20);
+    show();
+   
+       return 0;
 }
